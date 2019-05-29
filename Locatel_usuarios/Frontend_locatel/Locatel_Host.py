9801813cc0c63fa2134eeea6078e08bffa1a6d0b
Jhoany londoño <42972979+jlondo97@users.noreply.github.com>
2019-05-29 14:44:59 -0500

    Primer commit
    se agregaron las interfaces de usuario_login y una parte de usuario_puntos

---
+++ Locatel_usuarios/Frontend_locatel/Locatel_Host.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+app= Flask(__name__)
+
+@app.route("/")
+
+def _Login_():
+    return render_template('Usuario/LoginUsuario.html')
+
+@app.route("/puntos")
+def PuntosLocatel():
+    return  render_template('Usuario/VistaCliente.html')
+
+
+if __name__ == "__main__":
+    app.run(debug = True)    
