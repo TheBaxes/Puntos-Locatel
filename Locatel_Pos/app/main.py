3a35449abe497e7e34a719483288b9251639c3b6
Jhoany londoño <42972979+jlondo97@users.noreply.github.com>
2019-06-01 19:45:28 -0500

    POsInicial

--- Locatel_Pos/app/main.py
+++ Locatel_Pos/app/main.py
@@ -3,6 +3,11 @@ from flask import Blueprint, render_template, request
 main = Blueprint('main', __name__)
 
 
-@main.route("/")
+@main.route("/pos")
 def Consulta_venta_pos():
     return render_template('VistaPos.html')
+
+@main.route("/")
+
+def Seleccion_pais():
+    return render_template('PosInicial.html')
