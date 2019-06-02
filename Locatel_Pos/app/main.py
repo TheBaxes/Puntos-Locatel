9cbda340c4fd17e8ca825e4cbdd4056828b896cb
TheBaxes <sebas1475@gmail.com>
2019-06-01 19:47:26 -0500

    Fix port

--- Locatel_Pos/app/main.py
+++ Locatel_Pos/app/main.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request
+from .initialization import init_db
 
 main = Blueprint('main', __name__)
 
@@ -8,6 +9,10 @@ def Consulta_venta_pos():
     return render_template('VistaPos.html')
 
 @main.route("/")
-
 def Seleccion_pais():
     return render_template('PosInicial.html')
+
+@main.route("/init_db")
+def init():
+    init_db()
+    return "Database initialized"
